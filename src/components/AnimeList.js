611b60de5932b546f91ffb8b1021fef5631a51ed
Oumusik <siroshtandenchik2@gmail.com>
2022-04-11 17:38:27 +0300

    add aniAPI, searching

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -11,18 +11,46 @@ const AnimeList = () =>{
 
 
     useEffect(() => {
-        axios.get("https://cdn.animenewsnetwork.com/encyclopedia/reports.xml?id=155").then((result) =>{
-            setResults(result.anime.name)
+        axios.get(`https://api.aniapi.com/v1/anime?title=${term}`, {
+            headers:{
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE1ODUiLCJuYmYiOjE2NDk2ODE0NTcsImV4cCI6MTY1MjI3MzQ1NywiaWF0IjoxNjQ5NjgxNDU3fQ.JCE3Z29TGLLIzdDVF0V2kr4bMQNFwgI_qGnd3Gm1t9U',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }).then((result) =>{
+            setResults(result.data.data.documents)
         })
     },[term])
 
-
+    const renderedResults = results.map(result =>{
+        if(result.titles.en){
+            return( 
+                <li key={result.anilist_id}>
+                    <b>{result.titles.en}</b>
+                    <img src={result.cover_image}/>
+                </li>)
+        }
+        
+            return( 
+                <li key={result.anilist_id}>
+                    <b>{result.titles.rj}</b>
+                    <img src={result.cover_image}/>
+                </li>  
+            )
+        
+    })
     
 
 
     return (
         <div className="ui container">
-            <h1>{results.length}</h1>
+                    <div className="ui form">
+                        <div className="field">
+                            <label>Wpisz hasło Wikipedii</label>
+                            <input type="text" className="input" onChange={e => setTerm(e.target.value)} value={term}/>
+                        </div>
+                    </div>
+                <b>{renderedResults}</b>   
         </div>
     )
 
